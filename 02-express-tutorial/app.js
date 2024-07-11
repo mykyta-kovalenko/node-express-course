7c8873aab3ed22e640b3d8b134f7cc5c426450f6
Mykyta <mykyta.kovalenko@proarea.com>
2024-07-10 21:30:50 -0500

    practice middleware use

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,25 @@
-console.log('Express Tutorial')
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+const logger = (req, res, next) => {
+  const method = req.method;
+  const url = req.url;
+  console.log(method, url);
+  next();
+};
+
+app.use(logger);
+
+app.get('/', (req, res) => {
+  res.send('Home');
+});
+app.get('/about', (req, res) => {
+  res.send('About');
+});
+
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000....');
+});
